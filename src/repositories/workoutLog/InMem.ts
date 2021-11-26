0ccaa7b74f3904ace044daad8effef5fe450601a
ArieBisfki <arie_bisfki@live.nl>
2021-11-26 20:24:09 +0100

    - WorkoutLogRepository - Controllers resync with new repository return types - Exercise now contains ExerciseCategory object instead of id

--- src/repositories/workoutLog/InMem.ts
+++ src/repositories/workoutLog/InMem.ts
@@ -1,34 +1,46 @@
 import {exec, Result} from "../../utils/failOrSuccess";
 import WorkoutLog from "../../models/workout/WorkoutLog";
 import IWorkoutLogRepository from "./Interface";
-import E from "./Error";
+import WorkoutLogRepositoryError  from "./Error";
+import {ResistanceExerciseLog} from "../../models/workout/ResistanceExerciseLog";
+import {CardioExerciseLog} from "../../models/workout/CardioExerciseLog";
+import {exercises} from "../exercise/InMem";
 
+const E = WorkoutLogRepositoryError;
+type E = typeof E;
 type R<S, F> = Result<S, F>;
 
-export default class WorkoutLogRepositoryInMem implements IWorkoutLogRepository {
-    private workoutLogs: WorkoutLog[] = [];
+export const workoutLogs = [
+    new WorkoutLog(1,[
+        new ResistanceExerciseLog(exercises["Bench Press"],10,4,100),
+        new ResistanceExerciseLog(exercises.Squat,10,4,150),
+        new CardioExerciseLog(exercises.Running,30)
+    ], new Date())
+];
 
-    async create(workoutLog: WorkoutLog): Promise<R<WorkoutLog, E.DUPLICATE>> {
+export default class WorkoutLogRepositoryInMem implements IWorkoutLogRepository {
+    private workoutLogs: WorkoutLog[] = workoutLogs;
+
+    async create(workoutLog: WorkoutLog): Promise<R<WorkoutLog, E["DUPLICATE"]>> {
         return exec((resolve, err) => {
             const existingWorkoutLog = this.workoutLogs.find(w => w.id === workoutLog.id);
             if (existingWorkoutLog) {
                 return err(E.DUPLICATE);
             }
+
+            this.workoutLogs.push(workoutLog);
+
             resolve(workoutLog);
         });
     }
 
-    async get(id: number): Promise<R<WorkoutLog, E.NOT_FOUND>> {
+    async get(userId: number): Promise<R<WorkoutLog[], E["NOT_FOUND"]>> {
         return exec((resolve, err) => {
-            const workoutLog = this.workoutLogs.find(workoutLog => workoutLog.id === id);
-            if (!workoutLog) {
-                return err(E.NOT_FOUND);
-            }
-            resolve(workoutLog);
+            resolve(this.workoutLogs);
         });
     }
 
-    async update(workoutLog: WorkoutLog): Promise<R<WorkoutLog, E.NOT_FOUND>> {
+    async update(workoutLog: WorkoutLog): Promise<R<WorkoutLog, E["NOT_FOUND"]>> {
         return exec((resolve, err) => {
             const existingWorkoutLogIndex = this.workoutLogs.findIndex(w => w.id === workoutLog.id);
             if (existingWorkoutLogIndex === -1) {
@@ -39,7 +51,7 @@ export default class WorkoutLogRepositoryInMem implements IWorkoutLogRepository 
         });
     }
 
-    async delete(id: number): Promise<R<void, E.NOT_FOUND>> {
+    async delete(id: number): Promise<R<void, E["NOT_FOUND"]>> {
         return exec((resolve, err) => {
             const sizeBefore = this.workoutLogs.length;
             this.workoutLogs = this.workoutLogs.filter(workoutLog => workoutLog.id === id);
