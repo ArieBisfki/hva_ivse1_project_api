f589888c3163be8435178c2275ceba885941cdd4
S. Amaador <soufiane.amaador@hva.nl>
2021-12-07 14:19:23 +0100

    social group fully implemented w/ controller except query endpoint

---
+++ src/controller/social-group.ts
@@ -0,0 +1,68 @@
+import { container } from 'tsyringe';
+import { DI_TOKEN } from '../di/Registry';
+import { resultIsFail } from '../utils/FailOrSuccess';
+import { constants } from "http2";
+import { AddSocialGroupRequestHandler, DeleteSocialGroupRequestHandler, GetUserSocialGroupsRequestHandler, SocialGroupsByQueryRequestHandler, UpdateSocialGroupRequestHandler } from "../models/endpoint/social-group"
+
+const socialGroupRepository = container.resolve(DI_TOKEN.SocialGroupRepository);
+
+const addSocialGroup: AddSocialGroupRequestHandler = async(req, res, next) => {
+    const socialGroup = req.body;
+
+    const createResult = await socialGroupRepository.create(socialGroup);
+    if(resultIsFail(createResult)){
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send();
+    }else {
+        res.status(constants.HTTP_STATUS_OK).json({
+            socialGroup: socialGroup
+        }).send();
+    }
+
+}
+
+const deleteSocialGroup: DeleteSocialGroupRequestHandler = async(req, res, next) => {
+    const wasDeleted = await socialGroupRepository.delete(req.params.id);
+
+    const status = wasDeleted
+    ? constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
+    : constants.HTTP_STATUS_OK;
+    
+    res.status(status).send();
+}
+
+const updateSocialGroup: UpdateSocialGroupRequestHandler = async(req, res, next) => {
+    const socialgroup = req.body;
+
+    const updateResult = await socialGroupRepository.update(socialgroup);
+
+    if(resultIsFail(updateResult)) {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send();
+    } else {
+        res.status(constants.HTTP_STATUS_OK).json({
+            socialgroup: updateResult.result
+            
+        })
+    }
+}
+
+// TODO: hoe gaan we dit aanpakken?
+const getAllSocialGroupsByQuery: SocialGroupsByQueryRequestHandler = async(req, res, next) => {
+    
+}
+
+const getUserSocialGroups: GetUserSocialGroupsRequestHandler = async(req, res, next) => {
+    const socialGroups = await socialGroupRepository.get(req.params.id);
+
+    if (!socialGroups) {
+        res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send();
+    } else {
+        res.status(constants.HTTP_STATUS_OK)
+            .json({
+                socialGroups: socialGroups
+
+            })
+            .send();
+    }
+}
+
+export default {addSocialGroup, deleteSocialGroup, updateSocialGroup, getAllSocialGroupsByQuery, getUserSocialGroups};
